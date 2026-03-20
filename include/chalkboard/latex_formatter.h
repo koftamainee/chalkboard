8fa76c3204f13eea4bc4abf22c92597adff7bb1e
koftamainee <koftamainee@gmail.com>
2026-03-20 21:39:51 +0300

    fix formatting

--- include/chalkboard/latex_formatter.h
+++ include/chalkboard/latex_formatter.h
@@ -6,11 +6,11 @@
 #include <chalkboard/latex_serializable.h>
 
 
-  template <chalkboard::LatexSerializable T>
-  struct std::formatter<T> {
-    constexpr auto parse(auto& ctx) { return ctx.begin(); }
+template <chalkboard::LatexSerializable T>
+struct std::formatter<T> {
+  constexpr auto parse(auto& ctx) { return ctx.begin(); }
 
-    auto format(const T& obj, auto& ctx) const {
-      return std::format_to(ctx.out(), "\\({}\\)", obj.to_latex());
-    }
-  };
+  auto format(const T& obj, auto& ctx) const {
+    return std::format_to(ctx.out(), "\\({}\\)", obj.to_latex());
+  }
+};
