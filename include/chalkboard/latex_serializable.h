3438cadf34b3ae8565a2827814183da875f4f364
koftamainee <koftamainee@gmail.com>
2026-03-21 11:03:52 +0300

    change to_latex to be a free function

--- include/chalkboard/latex_serializable.h
+++ include/chalkboard/latex_serializable.h
@@ -3,8 +3,8 @@
 #include <string>
 
 namespace chalkboard {
-  template <typename T>
+  template<typename T>
   concept LatexSerializable = requires(const T& obj) {
-    { obj.to_latex() } -> std::convertible_to<std::string>;
+    { to_latex(obj) } -> std::convertible_to<std::string>;
   };
 }
