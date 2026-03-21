7ffe06aa83f462eeb111e105dbe0fb4af3eacd03
koftamainee <koftamainee@gmail.com>
2026-03-21 11:56:57 +0300

    should work

--- include/chalkboard/latex_formatter.h
+++ include/chalkboard/latex_formatter.h
@@ -2,15 +2,20 @@
 
 #include <format>
 #include <string>
+#include <type_traits>
 
 #include <chalkboard/latex_serializable.h>
 
-
 template <chalkboard::LatexSerializable T>
+  requires (!std::same_as<T, char> &&
+    !std::same_as<T, const char*> &&
+    !std::same_as<T, const void*>)
 struct std::formatter<T> {
-  constexpr auto parse(auto& ctx) { return ctx.begin(); }
+  constexpr auto parse(format_parse_context& ctx) {
+    return ctx.begin();
+  }
 
   auto format(const T& obj, auto& ctx) const {
     return std::format_to(ctx.out(), "\\({}\\)", to_latex(obj));
   }
-};
+}; // namespace std
